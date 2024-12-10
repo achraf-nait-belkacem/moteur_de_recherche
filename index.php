14593a01fb3db7e5ffabb845cc8f7bb5756e2357
achraf_nait_belkacem <achrafoonb@gmail.com>
2024-12-10 13:19:06 +0100

    css

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moteur de Recherche</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="search-container">
+        <h1>Moteur de Recherche</h1>
+        <form action="https://www.google.com/search" method="get">
+            <input type="text" name="q" class="search-bar" placeholder="Recherchez ici..." required>
+            <button type="submit" class="search-button">Rechercher</button>
+        </form>
+    </div>
+</body>
+</html>
